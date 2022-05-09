2a76f8001ffb4cf173e8ee431ee512f19e5d9c85
mdwiltfong <wiltfong.michael1@gmail.com>
2022-05-09 13:27:12 -0400

    setting up express testing

--- open-ai-app/src/express-server/server.test.js
+++ open-ai-app/src/express-server/server.test.js
@@ -1,7 +1,7 @@
 // src/mocks/server.js
 import { setupServer } from "msw/node";
 import { handlers } from "../mocks/handlers";
-
+const request = require("supertest");
 // This configures a request mocking server with the given request handlers.
 export const server = setupServer(...handlers);
 
@@ -19,4 +19,12 @@ beforeAll(() => {
 
 afterAll(() => {
   server.close();
+});
+
+describe("Express Tests", () => {
+  it("Express server is running", async () => {
+    const response = await request(app).get("/");
+    expect(response.statusCode).toEqual(200);
+    expect(response.body).toEqual("App is listening on port 8000");
+  });
 });
